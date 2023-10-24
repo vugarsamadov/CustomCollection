cc0a7b181fc38c572609476f4a52a25d0e6ceb31
Vugar Samadov <mi779pf7t@code.edu.az>
2023-10-24 23:15:51 +0400

    A

---
+++ CustomCollection.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using CustomCollection.List;
+
+
+IntList a = new IntList();
+
+a.Add(4); // 5
+a.Add(1); // 4
+a.Print();
+a.Add(new int[] { 3, 5 });
+a.Add(new int[] {2});
+a.Add(new int[] {});
+a.InsertionSort();
+//a.SelectionSort();
+Console.WriteLine();
+a.Print();
+Console.WriteLine(a.BinarySearch(2));
+Console.WriteLine(a.JumpSearch(2));
